0db086011997604567de3a0fa594c27d0a2bcd08
Елисеев Константин <eliseev-kv@itrev.ru>
2021-07-21 22:47:52 +0500

    Написал docker-compose.yaml добавил инициализацию базы данных

---
+++ docker/postgres/init/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+
+  CREATE ROLE core WITH
+  LOGIN
+  NOSUPERUSER
+  INHERIT
+  NOCREATEDB
+  NOCREATEROLE
+  NOREPLICATION
+  PASSWORD '1xbn1URIwB';
+
+  CREATE DATABASE coredb
+    WITH OWNER = core
+         ENCODING = 'UTF8'
+         TABLESPACE = pg_default
+         LC_COLLATE = 'en_US.utf8'
+         LC_CTYPE = 'en_US.utf8'
+         CONNECTION LIMIT = -1;
+  GRANT CONNECT, TEMPORARY ON DATABASE coredb TO public;
+
+  GRANT ALL ON DATABASE coredb TO core;
+
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username core coredb <<-EOSQL
+
+  CREATE SCHEMA sp
+      AUTHORIZATION core;
+
+  GRANT ALL ON SCHEMA sp TO core;
+EOSQL
+
